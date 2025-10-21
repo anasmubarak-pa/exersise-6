aeb268dfe60670c04832fb7688d1c550f64079b9
Anas-Mubarak <anasmubarak435@gmail.com>
2025-10-21 15:47:57 +0530

    task 5 modified

--- custom-filter-task-5/custom-filter.js
+++ custom-filter-task-5/custom-filter.js
@@ -1,22 +1,37 @@
-function myFilter(arr,func)
-{
-    let return_list = []
-    for(let element of arr)
-    {
-        if(func(element))
-        {
-            return_list.push(element)
-        }
-    }
-    return return_list
-}
+// function myFilter(arr,func)
+// {
+//     let return_list = []
+//     for(let element of arr)
+//     {
+//         if(func(element))
+//         {
+//             return_list.push(element)
+//         }
+//     }
+//     return return_list
+// }
 
 let test_array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 let greater_than = (a)=>a>10
 
-console.log(myFilter(test_array,greater_than))
+// console.log(myFilter(test_array,greater_than))
 
-// let v = new Object()
-// v.Prototype
+let Prototypeobj = {
+    myFilter(callback)
+    {
+        console.log(this)
+        let return_list = []
+        for(let element of this)
+        {
+            if(callback(element))
+            {
+                return_list.push(element)
+            }
+        }
+        return return_list
+    }
+}
 
-// console.log(v) 
+Object.assign(Array.prototype,Prototypeobj)
+
+console.log(test_array.myFilter(greater_than))
